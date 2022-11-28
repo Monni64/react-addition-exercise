b646f02bec22b5986fc2f87d1080038fafde3e72
Monni64 <onni.jonasson@gmail.com>
2022-11-28 12:07:40 +0200

    v0.0.1

--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,17 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 function App() {
-  const [number1, setNumber1] = React.useState();
-  const [number2, setNumber2] = React.useState();
+  const [number1, setNumber1] = React.useState(1);
+  const [number2, setNumber2] = React.useState(2);
 
   return (
     <div>
-      <input />
+      <input defaultValue={number1} />
       <span>+</span>
-      <input />
+      <input defaultValue={number2} />
 
       <span>=</span>
-      <span></span>
+      <span>{number1 + number2}</span>
     </div>
   );
 }
