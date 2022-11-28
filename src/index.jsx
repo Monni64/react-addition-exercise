02123d5a46a883029a388c52814c6e83aa463445
Monni64 <onni.jonasson@gmail.com>
2022-11-28 12:19:36 +0200

    v0.0.4

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 
 import './style.css';
 
+function addition(num1, num2) {
+	if (isNaN(num1) || isNaN(num2)) {
+		return "";
+	} else {
+		return (Number(num1) + Number(num2));
+	}
+}
+
 function App() {
   const [number1, setNumber1] = React.useState(1);
   const [number2, setNumber2] = React.useState(2);
@@ -14,7 +22,7 @@ function App() {
       <input defaultValue={number2} onChange={e => setNumber2(e.target.value)} />
 
       <span>=</span>
-      <span>{parseInt(number1) + parseInt(number2)}</span>
+      <span>{addition(number1, number2)}</span>
     </div>
   );
 }
